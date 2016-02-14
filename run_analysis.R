047bfba028f83c3fec4df227d4bdc50844d4ed12
Andris Strals <andris.strals@gmail.com>
2016-02-14 17:24:46 +0000

    Data download and unzip implemented

--- run_analysis.R
+++ run_analysis.R
@@ -4,3 +4,10 @@ if(!file.exists(dataDirName)) {
 	dir.create(dataDirName)
 }
 
+# Download and unzip source data
+zipUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+tmpFile <- tempfile()
+library(RCurl)
+download.file(zipUrl, destfile = tmpFile, method = "libcurl")
+unzip(tmpFile, exdir = dataDirName)
+
